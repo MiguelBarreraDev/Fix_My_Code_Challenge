b90d140ed6e3feaa611290e9b503b517905e23f0
MiguelBarreraDev <barreram987@gmail.com>
2022-04-24 22:15:19 -0500

    Betty styles

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -16,9 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int p;
 
 	if (*head == NULL)
-	{
 		return (-1);
-	}
 	saved_head = *head;
 	p = 0;
 	while (p < index && *head != NULL)
@@ -31,30 +29,21 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = saved_head;
 		return (-1);
 	}
-	if (0 == index)
+	if (index == 0)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 		if (tmp != NULL)
-		{
 			tmp->prev = NULL;
-		}
 	}
 	else
 	{
 		if ((*head)->next)
-		{
 			(*head)->next->prev = (*head)->prev;
-		}
 		(*head)->prev->next = (*head)->next;
 		free(*head);
 		*head = saved_head;
-		/* (*head)->prev->prev = (*head)->prev; */
-		/* free(*head); */
-		/* if ((*head)->next) */
-		/* 	(*head)->next->prev = (*head)->prev; */
-		/* *head = saved_head; */
 	}
 	return (1);
 }
